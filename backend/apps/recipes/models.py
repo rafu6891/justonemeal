92c5be746f0a37badce32666ab2611446d9df6d6
Rafu6891 <rafu6891@gmail.com>
2026-01-27 14:54:32 +0100

    Add recipe models and admin configuration

--- backend/apps/recipes/models.py
+++ backend/apps/recipes/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+#models de recetas, lo basico
+class Recipe(models.Model):
+    title = models.CharField(max_length=200) #nombre de la receta
+    description = models.TextField(blank=True) #descripcion de la receta
+    time_minutes = models.PositiveBigIntegerField() #tiempo en minutos
+    difficulty = models.CharField(max_length=20) #dificultad de preparacion
+    created_at = models.DateTimeField(auto_now_add=True) #creacion de la receta
+
+    def __str__(self):
+        return self.title
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+    unit = models.CharField(max_length=20) #para las unidades de medida
+
+    def __str__(self):
+        return self.name
+    
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='ingredients')
+    Ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.FloatField() #cantidad para 1 persona
+
+    def __str__(self):
+        return f"{self.quantity} {self.Ingredient.unit} {self.Ingredient.name}"
